f347b6fb4394f54bd5411c2402d2b1745ddc9241
Ainembabazi Nicholas <97317648+nicholas044@users.noreply.github.com>
2025-02-12 09:42:10 +0300

    Add files via upload

---
+++ config/middlewares.js
@@ -0,0 +1,18 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000'], // Add the URL of your React frontend
+      headers: ['Content-Type', 'Authorization'], // You can add any other necessary headers
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
